552b96266e3a555920fcf620256ce01bb250aae8
hernando-garcia <hernando.garcia@gmail.com>
2017-07-14 11:51:10 +0100

    Removed PythonPrograms/ dir

--- PythonPrograms/PigLatinTranslater.py
+++
@@ -1,7 +0,0 @@
-EnglishWord = raw_input ("What's the English word pls: ?")
-str(EnglishWord)
-firstletter = EnglishWord[0]
-everyotherletter = EnglishWord[1:]
-print everyotherletter + firstletter + "ay"
-
-
--- PythonPrograms/python_API_2.py
+++
@@ -1,7 +0,0 @@
-from urllib2 import urlopen
-
-kittens = urlopen('http://placekitten.com/200/300')
-
-f = open('kittens.jpg', 'wb')
-f.write(kittens.read())
-f.close()
